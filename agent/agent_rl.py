0d934dc09df28015f0b256d150b3cc27913f5d5e
grooleivsgard <gro.oleivsgard@gmail.com>
2024-09-18 16:48:25 +0200

    📝 Update hyperparameter configs

---
+++ agent/agent_rl.py
@@ -0,0 +1,263 @@
+import os
+import sys
+from collections import deque
+
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+# get the path to the project root directory and add it to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.append(project_root)
+# sys.path.append("../")
+
+import gymnasium as gym
+import keras as keras
+import numpy as np
+from neural_network_ff import NeuralNetworkFF
+
+import wandb
+from env import SunburstMazeDiscrete
+
+wandb.login()
+
+test_episodes = 100
+
+# Define the CSV file path relative to the project root
+map_path_train = os.path.join(project_root, "env/map_v0/map_closed_doors.csv")
+map_path_test = os.path.join(project_root, "env/map_v0/map_closed_doors.csv")
+
+def test_agent():
+
+    env = SunburstMazeDiscrete(map_path_test, render_mode="human")
+    state_shape = (env.observation_space.n,)
+    action_shape = (env.action_space.n,)
+
+    ql = NeuralNetworkFF()
+    # model = ql.agent(state_shape, action_shape)
+
+    # Load the old model
+    model = keras.models.load_model("model.keras")
+
+    total_reward = 0
+    render = True
+
+    for i in range(test_episodes):
+        state = env.reset()
+        done = False
+        total_reward = 0
+        print("Episode: ", i)
+        while not done:
+
+            if render:
+                env.render()
+
+            encoded = state.flatten()
+            # Normalized the state
+            env_size = env.width, env.height
+            encoded = ql.state_to_input(encoded, env_size)
+            encoded = encoded.flatten().reshape(1, -1)
+            print(model.predict(encoded), encoded)
+            action = np.argmax(model.predict(encoded))
+            print(action)
+
+            new_state, reward, done, _, info = env.step(action)
+            total_reward += reward
+            state = new_state
+
+    env.close()
+
+
+def train_agent():
+    # Hyperparameters to log
+    config = {
+        "loss_function": "mse",
+        "learning_rate": 0.001,
+        "batch_size": 64,
+        "optimizer": "adam",
+        "total_episodes": 3000,
+        "epsilon": 0.8,
+        "epsilon_decay": -0.005,
+        "epsilon_min": 0.1,
+        "discount_factor": 0.9,
+        "alpha": 0.01,
+        "map_path": map_path_train,
+        "target_model_update": 1600, # hard update of the target model
+        "max_steps_per_episode":800,
+        "random_start_position":True,
+        "replay_memory_size":deque(maxlen=2000),
+        "rewards" : {
+            "is_goal":1,
+            "hit_wall":-0.001,
+            "has_not_moved":0, # change to 0?
+            "new_square":0.01,
+            "max_steps_reached":-0.001,
+            "distance_to_goal":True
+        },
+
+        # TODO
+        "observation_space": {
+            "position":True,
+            "orientation":True,
+            "steps_to_goal":True,
+            "last_known_steps":0
+        },
+        # TODO
+        "layers": {
+            0: {"activation": "relu",
+                "nodes": 32
+                },
+            1: {"activation": "relu",   
+                "nodes": 32
+                },
+            2: {"activation": "relu", 
+                "nodes": 32
+                },
+            3: {"activation": "relu",
+                "nodes": 32
+                },
+            4: {
+                "activation": "linear",
+                # "nodes": action_shape[0]
+            }
+        }
+    }
+
+    render = True
+    epsilon = config.get("epsilon")
+    env = SunburstMazeDiscrete(
+        map_path_train, 
+        render_mode="human" if render else "none", 
+        max_steps_per_episode=config.get("max_steps_per_episode"), 
+        random_start_position=config.get("random_start_position"), 
+        rewards=config.get("rewards"), 
+        observation_space=config.get("observation_space"))
+    state_shape = (env.observation_space.n,)
+    action_shape = (env.action_space.n,)
+    env_size = (env.width, env.height)
+
+    # print(state_shape, action_shape)
+
+    ql = NeuralNetworkFF()
+
+    model = ql.agent(
+        state_shape,
+        action_shape,
+        config.get("loss_function"),
+        config.get("learning_rate"),
+    )
+    target_model = ql.agent(
+        state_shape,
+        action_shape,
+        config.get("loss_function"),
+        config.get("learning_rate"),
+    )
+
+
+    target_model.set_weights(model.get_weights())
+
+    replay_memory = config.get("replay_memory_size")
+
+    network_sync_rate_count = 0
+    total_reward = 0
+    step_count = 0
+
+    run, config = ql.start_run(project="sunburst-maze", config=config)
+
+    for i in range(config.get("total_episodes")):
+        state = env.reset()
+        print(
+            "=" * 100,
+            "\nRunning episode: ",
+            i,
+            "\nMouse position: ({}, {})".format(state[0], state[1]),
+            "\nMouse orientation:",
+            state[2],
+        )
+
+        done = False
+        total_reward = 0
+        total_rewards = []
+
+        step_count = 0
+
+        while not done:
+
+            if render:
+                env.render()
+
+            if np.random.rand() <= epsilon:
+                action = env.action_space.sample()
+                # print(action)
+            else:
+
+                encoded = state
+                encoded = ql.state_to_input(encoded, env_size)
+                encoded = encoded.flatten().reshape(1, -1)
+                action = np.argmax(model.predict(encoded))
+                # print(action)
+
+            new_state, reward, done, _, info = env.step(action)
+            # print("Reward: ", reward, "New State: ", new_state, "Done: ", done)
+            total_reward += reward
+            encoded = ql.state_to_input(state, env_size)
+            new_encoded = ql.state_to_input(new_state, env_size)
+            replay_memory.append(
+                [encoded.flatten(), action, reward, new_encoded.flatten(), done]
+            )
+
+            state = new_state
+
+            network_sync_rate_count += 1
+
+            step_count += 1
+
+            if done:
+                ql.train(
+                    replay_memory=replay_memory,
+                    model=model,
+                    target_model=target_model,
+                    done=done,
+                    batch_size=config.get("batch_size"),
+                    discount_factor=config.get("discount_factor"),
+                    alpha=config.get("alpha"),
+
+                )
+                # print(len(total_rewards))
+                total_rewards.append(total_reward)
+
+            
+                if network_sync_rate_count > config.get("target_model_update"):
+                    print("Updating target model")
+                    target_model.set_weights(model.get_weights())
+                    network_sync_rate_count = 0
+
+                # Decay epsilon
+                epsilon = (
+                    epsilon + config.get("epsilon_decay")
+                    if epsilon > config.get("epsilon_min")
+                    else config.get("epsilon_min")
+                )
+                print(f"Total Reward: {total_reward}, \nEpsilon: {epsilon}")
+                # ql.save_losses()
+
+        wandb.log({"Reward per episode": total_reward, "Epsilon decay": epsilon, "Number of steps per episode": step_count})
+
+        if i % 10 == 0 and i != 0:
+            print(f"Total Reward: {total_reward}, \nEpsilon: {epsilon:.2f}")
+            # wandb.log({"Total reward": total_reward, "Epsilon": epsilon})
+            # Save the model
+            model.save(f"model_episode_{i}.keras")
+
+        
+
+    # Save the model
+    model.save("model.keras")
+    ql.save_losses()
+    env.close()
+    ql.end_run(run)
+
+
+if __name__ == "__main__":
+    train_agent()
+    # test_agent()
---
+++ env/sunburstmaze_discrete.py
@@ -0,0 +1,455 @@
+import random as rd
+
+import gymnasium as gym
+import numpy as np
+import pygame
+from gymnasium import spaces
+from tqdm import tqdm
+
+from .AStar import astar
+from .file_manager import build_map
+from .Graph import Graph
+from .maze_game import Maze
+
+checkpoints = [
+    {"coordinates": [(19, 9), (19, 10), (19, 11)], "visited": False},
+    {"coordinates": [(13, 9), (13, 10), (13, 11)], "visited": False},
+    {"coordinates": [(7, 9), (7, 10), (7, 11)], "visited": False},
+    {"coordinates": [(5, 13), (6, 13), (7, 13)], "visited": False},
+    {"coordinates": [(13, 16), (13, 17), (13, 18), (13, 19)], "visited": False},
+]
+
+
+def action_encoding(action: int) -> str:
+
+    action_dict = {0: "forward", 1: "left", 2: "right"}
+
+    return action_dict[action]
+
+
+def build_step_length_map(env_map, goal):
+    graph = Graph(env_map)
+    env_graph = graph.make_graph()
+    print("Graph made")
+    # Iterate through every position in the map
+    steps_to_goal = np.zeros((env_map.shape[0], env_map.shape[1]))
+    for y in tqdm(range(env_map.shape[0])):
+        for x in range(env_map.shape[1]):
+            if env_map[y][x] == 1:
+                steps_to_goal[y][x] = -1
+                continue
+            # Calculate the distance to the goal from the current position
+            steps_to_goal[y][x] = calculate_a_star_distance(env_graph, (y, x), goal)
+            # Check if the distance above is more than 1 lower than the current position
+            if (
+                y > 0
+                and steps_to_goal[y - 1][x] < steps_to_goal[y][x] - 1
+                and env_map[y - 1][x] != 1
+                and x < env_map.shape[1] - 5
+            ):
+                steps_to_goal[y][x] = steps_to_goal[y - 1][x] + 1
+    return steps_to_goal
+
+
+def calculate_a_star_distance(graph, start, end):
+    a_star = astar(graph, start, end)
+    return len(a_star)
+
+
+class SunburstMazeDiscrete(gym.Env):
+
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 240}
+
+    def __init__(self, maze_file=None, render_mode=None, max_steps_per_episode=500, random_start_position=None, rewards=None, observation_space=None):
+        self.map_file = maze_file
+        self.env_map = build_map(maze_file)
+        self.height = self.env_map.shape[0]
+        self.width = self.env_map.shape[1]
+        self.random_start_position = random_start_position
+        self.rewards = rewards
+        self.observation_space = observation_space
+
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.env_map[y][x] == 2:
+                    self.goal = (y, x)
+                    break
+        print("height:", self.height, "width:", self.width, "goal:", self.goal)
+        # Create a graph
+        print("Building a A* map for calculating steps to goal")
+        self.steps_to_goal = build_step_length_map(
+            self.env_map, self.goal
+        )  # Comment this out for running with keyboard for faster loading
+        self.last_steps_to_goal = None
+        # self.steps_to_goal = np.zeros((self.env_map.shape[0], self.env_map.shape[1])) # Comment out for running with keyboard for faster loading
+        #print(self.steps_to_goal)
+
+        # Three possible actions: forward, left, right
+        self.action_space = spaces.Discrete(3)
+
+        self.observation_space = spaces.Discrete(3)  # Orientation + next_to_wall
+
+        self._action_to_direction = {
+            "forward": self.move_forward,
+            "left": self.turn_left,
+            "right": self.turn_right,
+        }
+
+        self.orientation = 0  # 0 = Up, 1 = Right, 2 = Down, 3 = Left
+
+        self.position = None
+        self.goal = None
+
+        # Episode step settings
+        self.max_steps_per_episode = max_steps_per_episode
+        self.steps_current_episode = 0
+
+        # Pygame and rendering
+        assert (
+            render_mode is None or render_mode in self.metadata["render_modes"]
+        ), f"Invalid render mode: {render_mode}"
+        self.render_mode = render_mode
+        self.render_maze = None
+        self.window = None
+        self.clock = None
+
+        ## Visited checkpoints
+        self.visited_squares = []
+        self.last_position = None
+        self.last_moves = []
+
+    def goal_position(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.env_map[y][x] == 2:
+                    return (y, x)
+        return None
+
+    def select_start_position(self) -> tuple:
+        """
+        Selects the start position for the maze.
+
+        Returns:
+            tuple: The coordinates of the selected start position.
+        """
+        position = (10, 13)
+
+        if self.random_start_position is True:
+            position = (rd.randint(0, self.height - 1), rd.randint(0, self.width - 1))
+            # Check if the position is not a wall
+            while int(self.env_map[position[0]][position[1]]) == 1:
+                position = (rd.randint(0, self.height - 1), rd.randint(0, self.width - 1))
+    
+        return position
+   
+    def _get_info(self):
+
+        return {"legal_actions": self.legal_actions(), "orientation": self.orientation}
+
+    def _get_observation(self):
+        steps_to_goal_at_position = self.steps_to_goal[self.position[0]][
+            self.position[1]
+        ]
+        return np.array([*self.position, self.orientation, steps_to_goal_at_position])
+
+    def reset(self, seed=None, options=None) -> tuple:
+
+        super().reset(seed=seed)
+
+        #self.visited_squares = []
+        # self.env_map = build_map(self.map_file)
+        self.position = self.select_start_position()
+        self.last_steps_to_goal = self.steps_to_goal[self.position[0]][self.position[1]]
+
+        self.steps_current_episode = 0
+
+        self.reset_checkpoints()
+        self.last_moves = []
+
+        # Render the maze
+        if self.render_mode == "human":
+            framerate = self.metadata["render_fps"]
+
+            self.render_maze = Maze(
+                self.map_file,
+                self.env_map,
+                self.width,
+                self.height,
+                framerate,
+                self.position,
+                self.orientation,
+            )
+        self.goal = self.goal_position()
+        return self._get_observation()
+
+    def reset_checkpoints(self):
+        for checkpoint in checkpoints:
+            checkpoint["visited"] = False
+
+    def can_move_forward(self) -> bool:
+        """
+        Determines whether the agent can move forward in the maze.
+        Returns:
+            bool: True if the agent can move forward, False otherwise.
+        """
+
+        # Get the coordinates of the cell in front of the agent
+        if self.orientation == 0:
+            next_position = (self.position[0] - 1, self.position[1])
+        elif self.orientation == 1:
+            next_position = (self.position[0], self.position[1] + 1)
+        elif self.orientation == 2:
+            next_position = (self.position[0] + 1, self.position[1])
+        elif self.orientation == 3:
+            next_position = (self.position[0], self.position[1] - 1)
+        else:
+            raise ValueError("Invalid orientation")
+
+        # Check if the cell in front of the agent is a wall
+        if int(self.env_map[next_position[0]][next_position[1]]) == 1:
+            return False
+
+        return True
+
+    def next_to_wall(self) -> list:
+        """
+        Determines which directions the agent is next to a wall.
+
+        Returns:
+            list: A list of binary values indicating whether the agent is next to a wall in each direction.
+                  The order of the values corresponds to [front, right, back, left].
+                  A value of 1 indicates that the agent is next to a wall in that direction,
+                  while a value of 0 indicates that the agent is not next to a wall in that direction.
+        """
+
+        next_to_wall = [0, 0, 0, 0]
+
+        # TODO: Clean this up
+        # Check if the cell in front of the agent is a wall
+        if int(self.env_map[self.position[0] - 1][self.position[1]]) == 1:
+            next_to_wall[0] = 1
+        # Check if the cell to the right of the agent is a wall
+        if int(self.env_map[self.position[0]][self.position[1] + 1]) == 1:
+            next_to_wall[1] = 1
+        # Check if the cell behind the agent is a wall
+        if int(self.env_map[self.position[0] + 1][self.position[1]]) == 1:
+            next_to_wall[2] = 1
+        # Check if the cell to the left of the agent is a wall
+        if int(self.env_map[self.position[0]][self.position[1] - 1]) == 1:
+            next_to_wall[3] = 1
+        return next_to_wall
+
+    def legal_actions(self) -> list:
+        """
+        Returns a list of legal actions that the agent can take.
+
+        Returns:
+            list: A list of legal actions. The agent can always turn left or right.
+            If the agent can move forward, "forward" is also included in the list.
+        """
+
+        # The agent can always turn left or right
+        actions = ["left", "right"]
+
+        if self.can_move_forward():
+            actions.append("forward")
+
+        return actions
+
+    def move_forward(self):
+        """
+        Moves the agent forward in the grid based on its current orientation.
+        The agent's position is updated according to its orientation:
+        - If the orientation is 0 (Up), the agent's position is decremented by 1 in the y-axis.
+        - If the orientation is 1 (Right), the agent's position is incremented by 1 in the x-axis.
+        - If the orientation is 2 (Down), the agent's position is incremented by 1 in the y-axis.
+        - If the orientation is 3 (Left), the agent's position is decremented by 1 in the x-axis.
+        """
+
+        if self.orientation == 0:  # Up
+            self.position = (self.position[0] - 1, self.position[1])
+
+        if self.orientation == 1:  # Right
+            self.position = (self.position[0], self.position[1] + 1)
+
+        if self.orientation == 2:  # Down
+            self.position = (self.position[0] + 1, self.position[1])
+
+        if self.orientation == 3:  # Left
+            self.position = (self.position[0], self.position[1] - 1)
+
+    def turn_left(self):
+        """
+        Turns the agent to the left.
+
+        This method updates the orientation of the agent by subtracting 1 from the current orientation and
+        taking the modulo 4 to ensure the orientation stays within the range of 0 to 3.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        self.orientation = (self.orientation - 1) % 4
+
+    def turn_right(self):
+        """
+        Turns the agent to the right.
+
+        This method updates the orientation of the agent by incrementing it by 1 and taking the modulo 4.
+        The modulo operation ensures that the orientation stays within the range of 0 to 3, representing the four
+        cardinal directions (north, east, south, west).
+        """
+        self.orientation = (self.orientation + 1) % 4
+
+    def step(self, action: int):
+        """
+        Takes a step in the environment based on the given action.
+        Parameters:
+            action (int): The action to take in the environment.
+        Returns:
+            observation (list): The current observation of the environment, legal actions.
+            reward (float): The reward obtained from the environment.
+            turnicated (bool):
+            terminated (bool): Whether the episode is terminated or not.
+            info (dict): Additional information about the environment.
+        """
+        self.last_moves.append(self.position)
+        # Used if the action is invalid
+        reward = self.reward()
+        observation = self._get_observation()
+        terminated = self.is_goal()
+        info = self._get_info()
+
+        if self.steps_current_episode >= self.max_steps_per_episode:
+            print("Reached max steps")
+            self.steps_current_episode = 0
+            return observation, self.rewards["max_steps_reached"], True, True, self._get_info()
+
+        action = action_encoding(action)
+        self.steps_current_episode += 1
+
+        # Walking into a wall
+        if action not in self.legal_actions():
+            # print("Hit a wall")
+            return observation, self.rewards["hit_wall"], False, False, info
+
+        # Perform the action
+        self._action_to_direction[action]()
+
+        # Updated values
+        reward = self.reward()
+        observation = self._get_observation()
+        terminated = self.is_goal()
+        info = self._get_info()
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, reward, terminated, False, info
+
+    def is_goal(self):
+        """
+        Checks if the current position is a goal position.
+        Returns:
+            bool: True if the current position is a goal position, False otherwise.
+        """
+        if int(self.env_map[self.position[0]][self.position[1]]) == 2:
+            return True
+        return False
+
+    def has_not_moved(self, position):
+        # Only keep the last 50 moves
+
+        if len(self.last_moves) < 10:
+            return False
+
+        self.last_moves = self.last_moves[-10:]
+        if all(last_move == position for last_move in self.last_moves):
+            return True
+        return False
+
+    def decreased_steps_to_goal(self):
+        """
+        Checks if the steps to the goal have decreased from the last position.
+
+        Returns:
+            bool: True if the steps to the goal have decreased, False otherwise.
+        """
+
+        current_steps_to_goal = self.steps_to_goal[self.position[0]][self.position[1]]
+
+        delta_steps = self.last_steps_to_goal - current_steps_to_goal
+        if delta_steps > 0:
+            return True
+        return False
+
+    def increased_steps_to_goal(self):
+        current_steps_to_goal = self.steps_to_goal[self.position[0]][self.position[1]]
+
+        delta_steps = self.last_steps_to_goal - current_steps_to_goal
+        if delta_steps < 0:
+            return True
+        return False
+
+    def distance_to_goal(self, position):
+        return np.sqrt(
+            (position[0] - self.goal[0]) ** 2 + (position[1] - self.goal[1]) ** 2
+        )
+
+    def distance_to_goal_reward(self):
+        max_distance = np.sqrt((self.height - 2) ** 2 + (self.width - 2) ** 2)
+
+        distance = self.distance_to_goal(self.position)
+        difference = max_distance - distance
+
+        reward = np.exp(-difference) * 5
+        discounted_reward = reward
+
+        return discounted_reward
+
+    # TODO: Gets stuck at wall, q-values for other actions are negative
+    def reward(self):
+        """
+        Calculates the reward for the current state.
+        Returns:
+            int: The reward value.
+        """
+
+        if self.is_goal():
+            print("Goal reached!")
+            return self.rewards["is_goal"]
+        
+        # TODO: Penalize for just rotating in place without moving
+        current_pos = self.position
+        if self.has_not_moved(self.position):
+            return self.rewards["has_not_moved"]
+       
+        # Update the last position
+        self.last_position = current_pos
+
+        # for checkpoint in checkpoints:
+        #     if self.position in checkpoint["coordinates"] and not checkpoint["visited"]:
+        #         checkpoint["visited"] = True
+        #         print("Checkpoint visited: ", self.position)
+        #         return 20
+        if self.decreased_steps_to_goal():
+            if self.rewards.get("distance_to_goal"):
+                return 0.01 + self.distance_to_goal_reward()
+
+        if self.position not in self.visited_squares:
+            self.visited_squares.append(self.position)
+            if self.rewards.get("distance_to_goal"):
+                return 0.01 + self.distance_to_goal_reward()
+
+        # if self.increased_steps_to_goal():
+        #    return -0.0005"
+
+        return 0
+
+    def render(self):
+        if self.render_mode == "rgb_array" or self.render_mode == "human":
+            return self._render_frame()
+
+    def _render_frame(self):
+        self.render_maze.draw_frame(self.env_map, s
