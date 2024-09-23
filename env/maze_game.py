bd39457ea29d57cf7f3866a73d836bab871cd768
henriklarsen28 <henrik.hl@hotmail.com>
2024-09-23 17:06:54 +0200

    ✨ feat: Improved ray placement:))

--- env/maze_game.py
+++ env/maze_game.py
@@ -13,9 +13,9 @@ grey = (192, 192, 192)
 
 
 
-NUMBER_OF_RAYS = 200
+NUMBER_OF_RAYS = 25
 RAY_LENGTH = 30 # VIEW_DISTANCE BLOCKS
-FIELD_OF_VIEW = math.pi / 4 # 90 degrees
+FIELD_OF_VIEW = math.pi / 1.1 # 180 degrees
 HALF_FOV = FIELD_OF_VIEW / 2
 STEP_ANGLE = FIELD_OF_VIEW / NUMBER_OF_RAYS
 
@@ -55,7 +55,7 @@ class Maze:
             pygame.init()
             pygame.display.set_caption("First Game")
             self.win = pygame.display.set_mode((screen_width, screen_height))
-
+        
         # load sprite
         script_dir = os.path.dirname(__file__)
         sprite_file = os.path.join(script_dir, "images", "orange_mouse_single.png")
@@ -74,6 +74,8 @@ class Maze:
         self.clock = pygame.time.Clock()
         self.framerate = framerate
         self.draw_frame(self.env_map, observed_squares_map, wall_rays)
+
+        self.marked_squares = set()
 
     def select_sprite(self, orientation: int):
         """
@@ -174,8 +176,37 @@ class Maze:
                 self.spriteHeight,
             ),
         )
+
+    def calculate_square_aheat(self, position: tuple, orientation: int):
+
+        if orientation == 0:
+            return (position[0] - 1, position[1])
+        elif orientation == 1:
+            return (position[0], position[1] + 1)
+        elif orientation == 2:
+            return (position[0] + 1, position[1])
+        elif orientation == 3:
+            return (position[0], position[1] - 1)
+        return position
+
+
     def draw_raycast(self, position: tuple, orientation):
         agent_angle = orientation * math.pi / 2 # 0, 90, 180, 270
+        position_ahead = self.calculate_square_aheat(position, orientation)
+        ray_shift_x = 0
+        ray_shift_y = 0
+        if orientation == 0:
+            ray_shift_y = 20
+            ray_shift_x= 40
+        elif orientation == 1:
+            ray_shift_y = 0
+            ray_shift_x = 20
+        elif orientation == 2:
+            ray_shift_y = 20
+            ray_shift_x = 0
+        elif orientation == 3:
+            ray_shift_y = 40
+            ray_shift_x = 20
 
         start_angle = agent_angle - HALF_FOV
         for ray in range(NUMBER_OF_RAYS):
@@ -183,11 +214,14 @@ class Maze:
                 x = int(position[0] - depth * math.cos(start_angle))
                 y = int(position[1] + depth * math.sin(start_angle))
                 if self.env_map[x][y] == 1:
-                    pygame.draw.line(self.win, (255,0,0), (position[1] * self.cell_size+20, position[0] * self.cell_size+20), (y * self.cell_size+15, x * self.cell_size+15))
+                    pygame.draw.line(self.win, (255,0,0), ((position_ahead[1] * self.cell_size) + ray_shift_y, (position_ahead[0] * self.cell_size) + ray_shift_x), (y * self.cell_size+15, x * self.cell_size+15))
                     break
+
+                marked_square = (x, y)
+                self.marked_squares.add(marked_square)
             start_angle += STEP_ANGLE
 
-            
+
 
 
 
