ca36085f2b1dbe9067298f32215d811f8e3235d5
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-09 11:13:08 +0200

    changed sequence length

--- agent/agent_rl.py
+++ agent/agent_rl.py
@@ -434,7 +434,7 @@ if __name__ == "__main__":
         "ray_length": 20,
         "number_of_rays": 100,
         "transformer": {
-            "sequence_length": 45,
+            "sequence_length": 20,
             "n_embd": num_states,
             "n_head": 8,
             "n_layer": 3,
