0170f3cc29b7986361f508dd127c1bf806afe161
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-16 08:34:40 +0200

    reduced embedding dimensions

--- agent/agent_rl_transformer.py
+++ agent/agent_rl_transformer.py
@@ -493,7 +493,7 @@ class Model_TrainTest:
 
                 action, att_weights_list = self.agent.select_action(tensor_sequence)
                 #print(att_weights_list[0][1])
-                block_1 = np.mean(np.stack(att_weights_list[0], axis=0), axis=0)
+                block_1 = np.mean(np.stack(att_weights_list[0], axis=0), axis=0) # TODO: Not sure if we should average this or just look at a single head.
                 #block_1 = att_weights_list[0][1]
                 last_attention_row = softmax(block_1[0,-1])
                 next_state, reward, done, truncation, _ = self.env.step(action)
@@ -599,7 +599,7 @@ if __name__ == "__main__":
         "number_of_rays": 100,
         "transformer": {
             "sequence_length": 15,
-            "n_embd": num_states,
+            "n_embd": 128,
             "n_head": 8,
             "n_layer": 3,
             "dropout": 0.2,
