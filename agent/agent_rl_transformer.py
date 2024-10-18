9da5dbb2a03f10b393eb22200f3e8b3b5bc6ba90
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-18 09:05:04 +0200

    increased sequence length

--- agent/agent_rl_transformer.py
+++ agent/agent_rl_transformer.py
@@ -582,8 +582,8 @@ if __name__ == "__main__":
         "ray_length": 20,
         "number_of_rays": 100,
         "transformer": {
-            "sequence_length": 15,
-            "n_embd": 256,
+            "sequence_length": 45,
+            "n_embd": 128,
             "n_head": 8,
             "n_layer": 3,
             "dropout": 0.3,
