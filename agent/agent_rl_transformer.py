a72e08c87729686d25f6208c77654015353ca73e
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-22 22:32:14 +0200

    feat: Changed from np to torch in salt pepper method

--- agent/agent_rl_transformer.py
+++ agent/agent_rl_transformer.py
@@ -103,10 +103,10 @@ def get_random_map():
 
 def salt_and_pepper_noise(matrix, prob=0.1):
 
-    goal_index = np.where(matrix == 2)
+    goal_index = torch.where(matrix == 2)
 
-    noisy_matrix = np.copy(matrix)
-    noise = np.random.rand(*matrix.shape)
+    noisy_matrix = torch.copy(matrix)
+    noise = torch.random.rand(*matrix.shape)
     noisy_matrix[noise < prob / 2] = 1  # Add "salt"
     noisy_matrix[noise > 1 - prob / 2] = 0  # Add "pepper"
     noisy_matrix[goal_index] = 2  # Ensure the goal is not obscured
