3d874f466b418ebc3250e5f378f3f66b1b6e41e6
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-24 14:08:24 +0200

    feat: cav file, not finished

---
+++ xai/build_dataset_cav.py
@@ -0,0 +1,16 @@
+# CAV for stuck in wall : Sees into wall and tries to move forward
+
+def negative_stuck_in_wall():
+    pass
+
+def positive_stuck_in_wall():
+    pass
+
+
+def negative_rotating_stuck():
+    pass
+
+def positive_rotating_stuck():
+    pass
+
+
