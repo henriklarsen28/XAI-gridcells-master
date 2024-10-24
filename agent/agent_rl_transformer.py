91627de03a2609dce26ec0db361b06f7e141d1b0
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-24 11:12:09 +0200

    reward shaping

--- agent/agent_rl_transformer.py
+++ agent/agent_rl_transformer.py
@@ -599,11 +599,11 @@ if __name__ == "__main__":
         "rewards": {
             "is_goal": 200 / 200,
             "hit_wall": -1 / 200,
-            "has_not_moved": -0.2 / 200,
+            "has_not_moved": -1 / 200,
             "new_square": 0.4 / 200,
             "max_steps_reached": -0.5 / 200,
             "penalty_per_step": -0.01 / 200,
-            "goal_in_sight": 5 / 200,
+            "goal_in_sight": 10 / 200,
         },
         # TODO
         "observation_space": {
