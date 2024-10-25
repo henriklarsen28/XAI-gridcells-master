af8d3294846444fb401cb8e7437dce99ebf41fcb
henriklarsen28 <henrik.hl@hotmail.com>
2024-10-25 11:40:16 +0200

    feat: stuck in wall cav

--- xai/cav/build_dataset_cav.py
+++ xai/cav/build_dataset_cav.py
@@ -3,6 +3,8 @@ import os
 import sys
 from collections import deque
 
+import torch
+
 # get the path to the project root directory and add it to sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -13,8 +15,8 @@ import torch
 from agent.dtqn_agent import DTQN_Agent
 from env import SunburstMazeDiscrete
 from utils.calculate_fov import calculate_fov_matrix_size
+from utils.sequence_preprocessing import add_to_sequence, padding_sequence
 from utils.state_preprocess import state_preprocess
-from utils.sequence_preprocessing import padding_sequence, add_to_sequence
 
 device = torch.device("cpu")
 fov_config = {
@@ -66,25 +68,45 @@ config = {
 # CAV for stuck in wall -> Sees into wall and tries to move forward
 
 
-def negative_stuck_in_wall(sequence: deque):
-    # Look at the last 3 states, if the agents last states are the same and the agent is not allowed to move forward the agent is stuck in a wall
+def negative_stuck_in_wall(sequence: deque, legal_actions: list):
+    # Look at the last 2 states, if the agents last states are the same and the agent is not allowed to move forward the agent is stuck in a wall
+    # The sequence should be added to the CAV negative dataset
+    last_state = sequence[-1]
+    second_last_state = sequence[-2]
+
+    if last_state != second_last_state and 0 in legal_actions:
+        return sequence
+    return None
+
+
+def positive_stuck_in_wall(sequence: deque, legal_actions: list):
+    # Look at the last 2 states, if the agents last states are the same and the agent is not allowed to move forward the agent is stuck in a wall
     # The sequence should be added to the CAV positive dataset
 
+    last_state = sequence[-1]
+    second_last_state = sequence[-2]
+
+    if last_state == second_last_state and 0 not in legal_actions:
+        # Save the observation sequence to the positive dataset
+        return sequence
+
+    return None
+
+
+def negative_rotating_stuck(
+    sequence: deque, action_sequence: deque, position_sequence: deque
+):
+
     pass
 
 
-def positive_stuck_in_wall(sequence: deque):
-    # Look at the last 3 states, if the agents last states are the same and the agent is not allowed to move forward the agent is stuck in a wall
+def positive_rotating_stuck(
+    sequence: deque, action_sequence: deque, position_sequence: deque
+):
+
+    # The position is the same over the last 12 states, and the agent is rotating in place
     # The sequence should be added to the CAV positive dataset
 
-    pass
-
-
-def negative_rotating_stuck():
-    pass
-
-
-def positive_rotating_stuck():
     pass
 
 
@@ -131,12 +153,27 @@ def build_csv_dataset():
     agent.model.load_state_dict(torch.load(model_load_path, map_location=device))
     agent.model.eval()
 
+    # Containing a tuple of observation sequence, legal_actions, position sequence, action sequence
+    collected_sequences = run_agent(env, agent)
+
+    for sequence in collected_sequences:
+        observation_sequence, legal_actions, position_sequence, action_sequence = sequence
+
+        # Check if the agent is stuck in a wall
+       
+
+
 
 def run_agent(env: SunburstMazeDiscrete, agent: DTQN_Agent):
+
+    collected_sequences = deque()
+
     sequence_length = config["transformer"]["sequence_length"]
     max_episodes = 100
-    sequence = deque(maxlen=sequence_length)
-
+    observation_sequence = deque(maxlen=sequence_length)
+    position_sequence = deque(maxlen=sequence_length)
+    action_sequence = deque(maxlen=sequence_length)
+    total_steps = 0
     # Testing loop over episodes
     for episode in range(1, max_episodes + 1):
         state, _ = env.reset(seed=42)
@@ -146,22 +183,29 @@ def run_agent(env: SunburstMazeDiscrete, agent: DTQN_Agent):
         total_reward = 0
 
         while not done and not truncation:
-            state = state_preprocess(state, device) # TODO: Import these, move them to utils
+            state = state_preprocess(state, device)
 
-            sequence = add_to_sequence(sequence, state, device)
-            tensor_sequence = torch.stack(list(sequence))
-            tensor_sequence = padding_sequence(
-                tensor_sequence, sequence_length, device
-            )
+            observation_sequence = add_to_sequence(observation_sequence, state, device)
+            position_sequence = add_to_sequence(position_sequence, env.position, device)
+            tensor_sequence = torch.stack(list(observation_sequence))
+            tensor_sequence = padding_sequence(tensor_sequence, sequence_length, device)
             print(tensor_sequence.shape)
             # q_val_list = generate_q_values(env=self.env, model=self.agent.model)
             # self.env.q_values = q_val_list
 
             action = agent.select_action(tensor_sequence)
+            action_sequence.append(action)
+            legal_actions = env.get_legal_actions()
             next_state, reward, done, truncation, _ = env.step(action)
             state = next_state
             total_reward += reward
             steps_done += 1
+            total_steps += 1
+            # Make sure the sequence length is filled up
+            if total_steps > sequence_length:
+                collected_sequences.append(
+                    (observation_sequence, legal_actions, position_sequence, action_sequence)
+                )
 
         # Print log
         result = (
@@ -170,3 +214,5 @@ def run_agent(env: SunburstMazeDiscrete, agent: DTQN_Agent):
             f"Reward: {total_reward:.2f}, "
         )
         print(result)
+
+    return collected_sequences
