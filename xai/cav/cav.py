3514fe7741f90554d3e1a4f725ff9f9be2dad8d8
henriklarsen28 <henrik.hl@hotmail.com>
2024-11-11 15:35:10 +0100

    wall concept

--- xai/cav/cav.py
+++ xai/cav/cav.py
@@ -296,11 +296,11 @@ def main():
     model_load_path = "../../agent/model/transformers/model_vivid-firebrand-872"
     #positive_file = "dataset/positive_wall_activations.pt"
     #negative_file = "dataset/negative_wall_activations.pt"
-    cav.calculate_cav("rotating", model_load_path)
-    cav.plot_cav("rotating")
+    cav.calculate_cav("wall", model_load_path)
+    cav.plot_cav("wall")
     
-    cav.calculate_random_cav("rotating", model_load_path)
-    cav.plot_cav("random")
+    #cav.calculate_random_cav("rotating", model_load_path)
+    #cav.plot_cav("random")
 
 
 if __name__ == "__main__":
