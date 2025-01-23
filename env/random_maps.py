c85377b91f4bf30d925b28e3857ce44b6675f638
henriklarsen28 <henrik.hl@hotmail.com>
2025-01-23 15:28:07 +0100

    feat: adding doors

--- env/random_maps.py
+++ env/random_maps.py
@@ -1,3 +1,5 @@
+import random as rd
+
 import numpy as np
 
 
@@ -10,21 +12,26 @@ def generate_random_maps(env_size: tuple):
     matrix = np.zeros(env_size)
 
     # Create the border walls
-    matrix[0, :] = 1
-    matrix[-1, :] = 1
-    matrix[:, 0] = 1
-    matrix[:, -1] = 1
+    matrix = add_border(matrix)
     print(matrix)
     # Generate a random map
     # 1. Generate random room sizes
     # Define how many rooms there should be
     room_count = env_size[0] // 10
 
-    room_sizes = []
+    rooms = []
     for i in range(room_count):
-        room_size = np.random.randint(env_size[1]//5, env_size[1]//2, 2)
-        print(room_sizes)
-        room_sizes.append(room_size)
+        room_size = np.random.randint(max(5,env_size[1] // 5), max(7,env_size[1] // 2), 2)
+
+        # Generate room matrises
+        room = np.zeros(room_size)
+        room = add_border(room)
+        room = add_doors(room, 1)
+        # Create a door in the room
+        rooms.append(room)
+        print(room)
+
+    #print(rooms)
     # 2. Generate random room positions
 
     # 3. Generate random corridor positions
@@ -32,6 +39,47 @@ def generate_random_maps(env_size: tuple):
     # 4. Generate random corridor sizes
 
     # 5. Generate random wall positions
+
+
+def add_border(matrix: np.array):
+    matrix[0, :] = 1
+    matrix[-1, :] = 1
+    matrix[:, 0] = 1
+    matrix[:, -1] = 1
+    return matrix
+
+
+def add_doors(matrix: np.array, number_of_doors: int):
+    sides_with_door = np.random.randint(0, 3, number_of_doors)
+
+    height, width = matrix.shape
+
+    for side in sides_with_door:
+        if side == 0:
+            # Top
+            door_width = max(width-4, np.random.randint(1, width // 2))
+            door_position = np.random.randint(1, width - door_width)
+            matrix[0, door_position : door_position + door_width] = 0
+
+        elif side == 1:
+            # Right
+            door_height = max(height-4, np.random.randint(1, height // 2))
+            door_position = np.random.randint(1, height - door_height)
+            matrix[door_position : door_position + door_height, -1] = 0
+
+        elif side == 2:
+            # Bottom
+            door_width = max(width-4, np.random.randint(1, width // 2))
+            door_position = np.random.randint(1, width - door_width)
+            matrix[-1, door_position : door_position + door_width] = 0
+
+        elif side == 3:
+            # Left
+            door_height = max(height-4, np.random.randint(1, height // 2))
+            door_position = np.random.randint(1, height - door_height)
+            matrix[door_position : door_position + door_height, 0] = 0
+
+    return matrix
 
 
 def generate_circular_maps(env_size: tuple):
@@ -50,7 +98,7 @@ def generate_circular_maps(env_size: tuple):
 
 def main():
     env_size = (21, 21)
-    #generate_circular_maps(env_size)
+    # generate_circular_maps(env_size)
     generate_random_maps(env_size)
 
 
