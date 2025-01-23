4e4fbfdbddb3b46b2ed978fb4d8b58b92fe40fb5
henriklarsen28 <henrik.hl@hotmail.com>
2025-01-23 12:46:21 +0100

    generate room sizes

--- env/random_maps.py
+++ env/random_maps.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def generate_random_maps(env_size: tuple):
 
     # Rules for the random map generation
@@ -16,10 +17,15 @@ def generate_random_maps(env_size: tuple):
     print(matrix)
     # Generate a random map
     # 1. Generate random room sizes
+    # Define how many rooms there should be
+    room_count = env_size[0] // 10
 
-
+    room_sizes = []
+    for i in range(room_count):
+        room_size = np.random.randint(env_size[1]//5, env_size[1]//2, 2)
+        print(room_sizes)
+        room_sizes.append(room_size)
     # 2. Generate random room positions
-
 
     # 3. Generate random corridor positions
 
@@ -27,27 +33,26 @@ def generate_random_maps(env_size: tuple):
 
     # 5. Generate random wall positions
 
+
 def generate_circular_maps(env_size: tuple):
     assert env_size[0] == env_size[1], "The environment size should be square"
     assert env_size[0] % 2 == 1, "The environment size should be odd"
     matrix = np.ones(env_size)
     center = (env_size[0] // 2, env_size[1] // 2)
 
-    for r in range(env_size[0]):
-        for c in range(env_size[1]):
-            if (r - center[0])**2 + (c - center[1])**2 <= center[0]**2:
+    for r in range(1, env_size[0] - 1):
+        for c in range(1, env_size[1] - 1):
+            if (r - center[0]) ** 2 + (c - center[1]) ** 2 <= center[0] ** 2:
                 matrix[r, c] = 0
 
-    matrix[0, :] = 1
-    matrix[-1, :] = 1
-    matrix[:, 0] = 1
-    matrix[:, -1] = 1
     print(matrix)
 
 
 def main():
     env_size = (21, 21)
-    generate_circular_maps(env_size)
+    #generate_circular_maps(env_size)
+    generate_random_maps(env_size)
+
 
 if __name__ == "__main__":
     main()
