540679827f7f4334e9aaa65e6848f38626655cff
henriklarsen28 <henrik.hl@hotmail.com>
2025-01-24 12:18:25 +0100

    lens update

--- agent/ppo.py
+++ agent/ppo.py
@@ -164,7 +164,7 @@ class PPO_agent:
                     "Reward per episode": rtgs.mean().item(),
                     "Policy loss": policy_loss.item(),
                     "Critic loss": critic_loss.item(),
-                    "Steps done": steps_done,
+                    "Steps done": lens[0],
                     "Gif:": (wandb.Video(gif, fps=4, format="gif") if gif else None),
                 },
                 commit=True,
