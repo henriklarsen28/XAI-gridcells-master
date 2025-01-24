9c7f0243e29392dd44fceb2a54273d40647dc1d7
henriklarsen28 <henrik.hl@hotmail.com>
2025-01-24 12:41:11 +0100

    conditional probability for creating a map

--- env/random_maps.py
+++ env/random_maps.py
@@ -2,6 +2,7 @@ import random as rd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def generate_random_maps(env_size: tuple):
@@ -34,6 +35,18 @@ def generate_random_maps(env_size: tuple):
 
     #print(rooms)
     # 2. Generate random room positions
+    # Find a position for the centre of the room
+    for room in rooms:
+        room_position = np.random.randint(1, env_size[0] - room.shape[0]), np.random.randint(1, env_size[1] - room.shape[1])
+        print(room_position)
+        # Check if the room fits in the map
+
+        
+
+        # Check if the room overlaps with another room
+        # If it does, generate a new room position
+
+
 
         # Generate room matrises
         room = np.zeros(room_size)
@@ -226,6 +239,31 @@ def add_doors(matrix: np.array, number_of_doors: int):
 
     return matrix
 
+def generate_random_map_conditional_prob(env_size: tuple, prob: float):
+    matrix = np.zeros(env_size, dtype=int)
+    
+    for i in range(1, env_size[0]-1):
+        for j in range(1, env_size[1]-1):
+            # Calculate the number of walls around
+            neighbor_walls = sum([
+                matrix[i-1, j], matrix[i+1, j],
+                matrix[i, j-1], matrix[i, j+1]
+            ])
+            # Reduce the sum if there is a wall diagonally
+            diagonal_walls = sum([
+                matrix[i-1, j-1], matrix[i-1, j+1],
+                matrix[i+1, j-1], matrix[i+1, j+1]
+            ])
+            
+            # Increase the probability of generating a wall if there are many walls around
+            adjusted_prob = prob + 0.15 * neighbor_walls - 0.05 * diagonal_walls
+            
+            if rd.random() < adjusted_prob:
+                matrix[i, j] = 1
+
+    # Ytterkantene som vegger
+    matrix = add_border(matrix)
+    return matrix
 
 def generate_circular_maps(env_size: tuple):
     assert env_size[0] == env_size[1], "The environment size should be square"
