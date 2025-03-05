5fca8b57ea4a834bcd5a750ddc730b38d6a6f74f
henriklarsen28 <henrik.hl@hotmail.com>
2025-03-05 16:13:23 +0100

    3 layers

--- agent/ppo/agent_ppo_transformer.py
+++ agent/ppo/agent_ppo_transformer.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
             "sequence_length": 30,
             "n_embd": 196,
             "n_head": 8,
-            "n_layer": 2,
+            "n_layer": 3,
             "dropout": 0.2,
             "decouple_positional_embedding": False,
         },
